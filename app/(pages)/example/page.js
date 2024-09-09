521904f87f7889e3429e765704f8bf0ce8ac1860
avrahamvais123 <avrahamvais123@gmail.com>
2024-09-09 18:19:21 +0300

    try to ipload to vercel

--- app/(pages)/example/page.js
+++ app/(pages)/example/page.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Page = () => {
-  return <div className="size-full flex-col-center">Example</div>;
+  return <div className="size-full flex-col-center">Example 2</div>;
 };
 
 export default Page;
